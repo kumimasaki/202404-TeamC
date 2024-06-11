75cfa9f1a9fa079d585ec76957e51bd36b990f88
BIRDYWONG <birdywang95@gmail.com>
2024-06-11 18:11:37 +0900

    更新0611

--- チーム開発/JavaCode/ReportProject/src/main/java/rp/com/controllers/UserListController.java
+++ チーム開発/JavaCode/ReportProject/src/main/java/rp/com/controllers/UserListController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import jakarta.servlet.http.HttpSession;
 import rp.com.services.UserService;
 import rp.com.models.entity.Users;
 
@@ -16,41 +15,37 @@ import java.util.List;
 @Controller
 public class UserListController {
 
-    @Autowired
-    private HttpSession session;
+	@Autowired
+	private UserService userService;
 
+	// ユーザー一覧ページを表示するメソッド
+	@GetMapping("/user/list")
+	public String showUserList(Model model) {
+		// すべてのユーザーを取得
+		List<Users> usersList = userService.getAllUsers();
+		// 取得したユーザーリストをモデルに追加
+		model.addAttribute("usersList", usersList);
+		// user_list.htmlテンプレートを返す
+		return "user_list.html";
+	}
 
-    @Autowired
-    private UserService userService;
+	// ユーザー検索を処理するメソッド
+	@PostMapping("/user/search")
+	public String searchUsers(@RequestParam("search") String search, Model model) {
+		// 名前またはメールアドレスでユーザーを検索
+		List<Users> usersList = userService.searchUsersByNameOrEmail(search);
+		// 検索結果をモデルに追加
+		model.addAttribute("usersList", usersList);
+		// user_list.htmlテンプレートを返す
+		return "user_list";
+	}
 
-    // ユーザー一覧ページを表示するメソッド
-    @GetMapping("/user/list")
-    public String showUserList(Model model) {
-        // すべてのユーザーを取得
-        List<Users> usersList = userService.getAllUserList();
-        // 取得したユーザーリストをモデルに追加
-        model.addAttribute("usersList", usersList);
-        // user_list.htmlテンプレートを返す
-        return "user_list";
-    }
-
- // ユーザー検索を処理するメソッド
-    @PostMapping("/user/search")
-    public String searchUsers(@RequestParam("search") String search, Model model) {
-        // 名前またはメールアドレスでユーザーを検索
-        List<Users> usersList = userService. searchUsersByNameOrEmail(search);
-        // 検索結果をモデルに追加
-        model.addAttribute("usersList", usersList);
-        // user_list.htmlテンプレートを返す
-        return "user_list";
-    }
-  
-    // ユーザーを削除するメソッド
-    @PostMapping("/user/delete")
-    public String deleteUser(@RequestParam("userId") Long userId) {
-        // 指定されたIDのユーザーを削除
-        userService.deleteUser(userId);
-        // ユーザー一覧ページにリダイレクトする
-        return "redirect:/user/list";
-    }
+	// ユーザーを削除するメソッド
+	@PostMapping("/user/delete")
+	public String deleteUser(@RequestParam("userId") Long userId) {
+		// 指定されたIDのユーザーを削除
+		userService.deleteUser(userId);
+		// ユーザー一覧ページにリダイレクトする
+		return "redirect:/user/list";
+	}
 }
