b30e3cecb04aafdc0953bb42e053200e3a38a7ba
liuqingwan0612 <haiyanliu211@gmail.com>
2024-06-13 19:14:21 +0900

    0613冲突

--- チーム開発/JavaCode/ReportProject/src/main/resources/static/js/user_report_create.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("report-create-button").addEventListener("click", function() {
-        window.location.href = "/user/report/create";
-    });
-});
